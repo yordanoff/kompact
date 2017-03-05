5761ded272f6bfa80ec31a27c52af8b9e72e8dc2
Joro Yordanov <Yordanov@yordanoff.local>
2017-03-05 18:11:05 +0200

    - improved line-height - improved default.php markup - improved blockquotes - improved article width in order to achieve better readibility apprx. 10 words per line

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,26 +1,36 @@
 <?php snippet('header') ?>
 
 <div class="wrap-fluid">
-    <h1><?= $page->title()->kirbytext() ?></h1>
-    <div>
-        <?= $page->text()->kirbytext() ?>
+
+    <article class="article index">
+
+        <header>
+            <h1><?= $page->title()->kirbytext() ?></h1>
+        </header>
+
+        <div class="text">
+            <?= $page->text()->kirbytext() ?>
+        </div>
+
         <?php if( page('blog') ): ?>
             <br />
+
             <p class="article-date">
                 <?= $page->date('F jS, Y') ?>
             </p>
         <?php endif; ?>
-    </div>
+
+    </article>
 
     <hr />
 
     <?php if( page('blog') ): ?>
         <?php if($page->hasNextVisible() ): ?>
             <strong>Read Next</strong> <br />
-            <a href="<?= $page->nextVisible()->url(); ?>" class="h1"><?= $page->nextVisible()->title(); ?></a>&rarr;
+            <a href="<?= $page->nextVisible()->url(); ?>" class="h1"><?= $page->nextVisible()->title(); ?></a> &rarr;
         <?php else: ?>
             <strong>End.</strong> <br />
-            <a href="<?= $site->url(); ?>" class="h1">Start over</a>&rarr;
+            <a href="<?= $site->url(); ?>" class="h1">Start over</a> &rarr;
         <?php endif; ?>
     <?php endif; ?>
 
